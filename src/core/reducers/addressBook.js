2b6c2a0131289058bb71fd0380b3f36d230276c4
Lotte Koblens <lotte.koblens@hva.nl>
2023-05-20 15:39:51 +0200

    primary and secondary classes are added

--- src/core/reducers/addressBook.js
+++ src/core/reducers/addressBook.js
@@ -6,7 +6,8 @@ const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case "address/add":
       /** TODO: Prevent duplicate addresses */
-      return { ...state, addresses: [...state.addresses, action.payload] };
+      return { ...state, addresses: [...state.addresses, action.payload] }
+    // return { ...state, addresses: [...state.addresses, action.payload] };
     case "address/remove":
       /** TODO: Write a state update which removes an address from the addresses array. */
       return { ...state, addresses: state.addresses.filter((event) => event.id !== action.payload) }
